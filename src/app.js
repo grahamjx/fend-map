fba10130aaeb6f8c732a8d497ac67c6d53b78034
slkcoin <xcoin@me.com>
2016-05-23 09:55:31 -0700

    filter markers issue

--- src/app.js
+++ src/app.js
@@ -1,9 +1,4 @@
-function stringStartsWith(string, startsWith) {
-  string = string || "";
-  if (startsWith.length > string.length)
-    return false;
-  return string.substring(0, startsWith.length) === startsWith;
-};
+var map;
 
 var places = [
   {"name": "Island Taqueria",
@@ -18,21 +13,60 @@ var places = [
    "venue": "4645be7ef964a5207f461fe3"}
 ];
 
-/* Class to represent a row in the seat reservations grid
-function restaurant (name) {
-    this.name = name;
-    //self.meal = ko.observable(initialMeal);
-}*/
+function initMap(){
+  map = new google.maps.Map(document.getElementById("map"), {
+  center: {lat: 37.7652065, lng: -122.2416355},
+  zoom: 13});
 
+  /*places.forEach(function(place) {
+  $.getJSON('https://api.foursquare.com/v2/venues/'+ place.venue + '?client_id=J5B15DIFBQULDELDRC00BET5PTEUKTEFUMFDZ5HAYSY2P33R&client_secret=XIH1G3153DXNXBNSEFUEHFCPTMY0YVAGK5LWGZJQOQFQKLMY&v=20130815&ll=37.7,-122',
+    function (data) {
+      var location = {lat: data.response.venue.location.lat, lng: data.response.venue.location.lng}
+      var name = data.response.venue.name;
+      place.marker = new google.maps.Marker({
+        map: map,
+        position: location,
+        title: name
+      });
+      place.infoWindow = new google.maps.InfoWindow({
+        content: name
+      });
+
+      google.maps.event.addListener(place.marker, 'click',function() {
+        map.setCenter(place.marker.position);
+        map.setZoom(16);
+        place.infoWindow.open(map, place.marker);
+      });
+        //map.setCenter(place.marker.position);
+        //map.setZoom(16);
+        //place.infoWindow.open(map, place.marker);
+        //self.handleThis(place.marker, place.infoWindow));
+
+      google.maps.event.addListener(place.infoWindow, 'closeclick', function() {
+        map.setCenter({lat: 37.7652065, lng: -122.2416355});
+        map.setZoom(13);
+      });
+    });
+});*/
+
+  ko.applyBindings(new listViewModel());
+}
+
+function stringStartsWith(string, startsWith) {
+  string = string || "";
+  if (startsWith.length > string.length)
+    return false;
+  return string.substring(0, startsWith.length) === startsWith;
+}
 
 // Overall viewmodel for this screen, along with initial state
-var listViewModel = function(places) {
+function listViewModel() {
   var self = this;
 
   self.itemToFilter = ko.observable('');
   self.placesArray = ko.observableArray(places);
 
-  self.placesArray().forEach(function(place) {
+    self.placesArray().forEach(function(place) {
     $.getJSON('https://api.foursquare.com/v2/venues/'+ place.venue + '?client_id=J5B15DIFBQULDELDRC00BET5PTEUKTEFUMFDZ5HAYSY2P33R&client_secret=XIH1G3153DXNXBNSEFUEHFCPTMY0YVAGK5LWGZJQOQFQKLMY&v=20130815&ll=37.7,-122',
       function (data) {
         var location = {lat: data.response.venue.location.lat, lng: data.response.venue.location.lng}
@@ -89,9 +123,16 @@ var listViewModel = function(places) {
     google.maps.event.trigger(place.marker, 'click');
   }
 
+  self.resetMarkers = function() {
+        self.placesArray().forEach(function(place){
+          place.marker.setVisible(true);
+        })
+    };
+
   self.filteredItems = ko.computed(function() {
     var filter = self.itemToFilter().toLowerCase();
     if (!filter) {
+        //self.resetMarkers();
         return self.placesArray();
     } else {
         return(ko.utils.arrayFilter(self.placesArray(), function(place) {
@@ -102,5 +143,3 @@ var listViewModel = function(places) {
     }
   });
 }
-var vm = new listViewModel(places);
-ko.applyBindings(vm);
