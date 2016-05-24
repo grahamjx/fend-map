b2a677da6f5a370660389c396eaccf6f68f4f2c3
slkcoin <xcoin@me.com>
2016-05-23 20:16:59 -0700

    minor fixes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 var map;
 
-var places = [
+var placeData = [
   {"name": "Island Taqueria",
    "venue": "4dd857ab2271c5d36d52eaf0"},
   {"name": "South Shore Cafe",
@@ -18,37 +18,6 @@ function initMap(){
   center: {lat: 37.7652065, lng: -122.2416355},
   zoom: 13});
 
-  /*places.forEach(function(place) {
-  $.getJSON('https://api.foursquare.com/v2/venues/'+ place.venue + '?client_id=J5B15DIFBQULDELDRC00BET5PTEUKTEFUMFDZ5HAYSY2P33R&client_secret=XIH1G3153DXNXBNSEFUEHFCPTMY0YVAGK5LWGZJQOQFQKLMY&v=20130815&ll=37.7,-122',
-    function (data) {
-      var location = {lat: data.response.venue.location.lat, lng: data.response.venue.location.lng}
-      var name = data.response.venue.name;
-      place.marker = new google.maps.Marker({
-        map: map,
-        position: location,
-        title: name
-      });
-      place.infoWindow = new google.maps.InfoWindow({
-        content: name
-      });
-
-      google.maps.event.addListener(place.marker, 'click',function() {
-        map.setCenter(place.marker.position);
-        map.setZoom(16);
-        place.infoWindow.open(map, place.marker);
-      });
-        //map.setCenter(place.marker.position);
-        //map.setZoom(16);
-        //place.infoWindow.open(map, place.marker);
-        //self.handleThis(place.marker, place.infoWindow));
-
-      google.maps.event.addListener(place.infoWindow, 'closeclick', function() {
-        map.setCenter({lat: 37.7652065, lng: -122.2416355});
-        map.setZoom(13);
-      });
-    });
-});*/
-
   ko.applyBindings(new listViewModel());
 }
 
@@ -64,20 +33,18 @@ function listViewModel() {
   var self = this;
 
   self.itemToFilter = ko.observable('');
-  self.placesArray = ko.observableArray(places);
+  self.placesArray = ko.observableArray(placeData);
 
     self.placesArray().forEach(function(place) {
     $.getJSON('https://api.foursquare.com/v2/venues/'+ place.venue + '?client_id=J5B15DIFBQULDELDRC00BET5PTEUKTEFUMFDZ5HAYSY2P33R&client_secret=XIH1G3153DXNXBNSEFUEHFCPTMY0YVAGK5LWGZJQOQFQKLMY&v=20130815&ll=37.7,-122',
       function (data) {
-        var location = {lat: data.response.venue.location.lat, lng: data.response.venue.location.lng}
-        var name = data.response.venue.name;
         place.marker = new google.maps.Marker({
           map: map,
-          position: location,
-          title: name
+          position: {lat: data.response.venue.location.lat, lng: data.response.venue.location.lng},
+          title: data.response.venue.name
         });
         place.infoWindow = new google.maps.InfoWindow({
-          content: name
+          content: data.response.venue.name
         });
 
         google.maps.event.addListener(place.marker, 'click',
@@ -119,22 +86,21 @@ function listViewModel() {
 		};
 	};
 
-  self.markerClick = function(place){
-    google.maps.event.trigger(place.marker, 'click');
-  }
-
   self.resetMarkers = function() {
         self.placesArray().forEach(function(place){
-          place.marker.setVisible(true);
-        })
+            place.marker.setVisible(true);
+          });
     };
 
   self.filteredItems = ko.computed(function() {
     var filter = self.itemToFilter().toLowerCase();
     if (!filter) {
-        //self.resetMarkers();
+      if (self.placesArray()[0].hasOwnProperty('marker')) {
+        self.resetMarkers();
+      }
         return self.placesArray();
     } else {
+      self.resetMarkers();
         return(ko.utils.arrayFilter(self.placesArray(), function(place) {
             var visible = stringStartsWith(place.name.toLowerCase(), filter);
             place.marker.setVisible(visible);
