6b76a5514445147515f0da56f8771e7587aa4e00
slkcoin <xcoin@me.com>
2016-06-05 09:35:31 -0700

    ui changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,11 @@
+//This is my main JavaScript file used from the map application.
+
 var map,
     FOURSQUARE_API_CLIENT = 'J5B15DIFBQULDELDRC00BET5PTEUKTEFUMFDZ5HAYSY2P33R',
     FOURSQUARE_API_SECRET = 'XIH1G3153DXNXBNSEFUEHFCPTMY0YVAGK5LWGZJQOQFQKLMY',
+
+/* Starting data. Only contains the name as a reference for myself. All the information
+  including location is pull from the foursquare api. */
 
     placeData = [
   {"name": "Island Taqueria",
@@ -16,6 +21,7 @@ var map,
 ];
 
 
+// Creates the map and sets a comfortable center point
 function initMap(){
   map = new google.maps.Map(document.getElementById("map"), {
   center: {lat: 37.7652065, lng: -122.2416355},
@@ -25,6 +31,7 @@ function initMap(){
   ko.applyBindings(new listViewModel());
 }
 
+//Helper function that compares string values used in the filtering function
 function stringStartsWith(string, startsWith) {
   string = string || "";
   if (startsWith.length > string.length)
@@ -32,12 +39,18 @@ function stringStartsWith(string, startsWith) {
   return string.substring(0, startsWith.length) === startsWith;
 }
 
-// Overall viewmodel for this screen, along with initial state
+// Overall viewmodel
 function listViewModel() {
   var self = this;
+  var lastInfoWindow = null;
 
   self.itemToFilter = ko.observable('');
-  self.placesArray = ko.observableArray(placeData);
+  self.placesArray = ko.observableArray(placeData); //contains the orignal model data
+
+  /* For each place in the array, it runs a getJSON call to the foursquare api using my account information.
+     When it's done, it takes the data and creates a corresponding map marker and info window containing the
+     specificed information. It's important to note, that the marker placement uses the location provided by
+     fourquare not googlePlaces data. */
 
   self.placesArray().forEach(function(place) {
 
@@ -54,7 +67,7 @@ function listViewModel() {
       });
 
 
-      var contentString = '<div>'+
+      var contentString = '<div class="infowindow">'+
       '<h1>'+ response.name +'</h1>'+ '<div id="pic"><img src="' +
       response.bestPhoto.prefix + '210x210' + response.bestPhoto.suffix +
       '" alt="Image Location"></div><p><b>Category: </b>' + response.categories[0].name + '</p><p>' + description + '</p><p><b>Phone: </b>' +
@@ -68,17 +81,21 @@ function listViewModel() {
       });
 
       google.maps.event.addListener(place.marker, 'click', self.pinAnimation(place.marker, place.infoWindow));
+
+      google.maps.event.addListener(place.infoWindow, 'closeclick', function() {
+        lastInfoWindow = null;
+      });
     })
 
+    //If the API isn't working, it will ale
     .fail(function() {
-      alert("Error loading Foursquare API Data! Please see developer console for more information.");
+      alert("Error loading Foursquare API Data! Map marker could not be created. Please see developer console for more information.");
     })
-
   });
 
-  var lastInfoWindow = null;
   self.pinAnimation = function(marker, infoWindow) {
 		function bounce() {
+      map.setCenter(marker.getPosition());
 			marker.setAnimation(google.maps.Animation.BOUNCE);
 			setTimeout(function(){marker.setAnimation(null); }, 1900);
 		};
