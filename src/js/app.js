e01683ee26397ceab440e779b9ebd7f5f10a98d6
slkcoin <xcoin@me.com>
2016-06-06 01:55:29 -0700

    fixes

--- src/js/app.js
+++ src/js/app.js
@@ -30,64 +30,35 @@ var map,
      "lng": -122.23850548267365}
    ];
 
-
 // Creates the map and sets a comfortable center point
 function initMap(){
   map = new google.maps.Map(document.getElementById("map"), {
   disableDefaultUI: true});
 
   window.mapBounds = new google.maps.LatLngBounds();
+  var infoWindow = new google.maps.InfoWindow();
 
   //event listener to handle window resize
   google.maps.event.addDomListener(window, "resize", function() {
     map.fitBounds(mapBounds);
-});
+  });
 
   ko.applyBindings(new listViewModel());
 }
 
-//Helper function that compares string values used in the filtering function
-function stringStartsWith(string, startsWith) {
-  string = string || "";
-  if (startsWith.length > string.length)
-    return false;
-  return string.substring(0, startsWith.length) === startsWith;
+function googleError() {
+  alert("Failed to access Google Maps API. Please check your connection and try again!");
 }
 
 // Overall viewmodel
 function listViewModel() {
   var self = this;
   var lastInfoWindow = null;
+  var infoWindow = new google.maps.InfoWindow();
 
   self.itemToFilter = ko.observable('');
   self.placesArray = ko.observableArray(placeData); //contains the orignal model data
 
-//Function is used to animate the markers and open/close info windows appropriately
-  self.pinAnimation = function(marker, infoWindow) {
-		function bounce() {
-      map.setCenter(marker.getPosition());
-			marker.setAnimation(google.maps.Animation.BOUNCE);
-			setTimeout(function(){marker.setAnimation(null); }, 1400);
-		}
-
-		return function() {
-			if (lastInfoWindow === infoWindow) {
-        bounce(marker);
-	      infoWindow.close(map, this);
-        map.panTo(marker);
-	      lastInfoWindow = null;
-      }
-	    else {
-	      if(lastInfoWindow !== null) {
-	      lastInfoWindow.close(map, this);
-	      bounce(marker);
-        }
-      bounce(marker);
-	    infoWindow.open(map, this);
-      lastInfoWindow = infoWindow;
-	    }
-		};
-	};
 
 /* For each place in the array, I create a simple map marker and info window. The
    marker uses the static information found in the starting model data. After the
@@ -106,26 +77,14 @@ function listViewModel() {
       title: place.name
     });
 
-    place.infoWindow = new google.maps.InfoWindow({
-      content: place.name
-    });
-
-    google.maps.event.addListener(place.marker, 'click', self.pinAnimation(place.marker, place.infoWindow));
-
-    google.maps.event.addListener(place.infoWindow, 'closeclick', function() {
-      lastInfoWindow = null;
-      map.fitBounds(mapBounds);
-    });
-
     $.getJSON('https://api.foursquare.com/v2/venues/'+ place.venue + '?client_id='+ FOURSQUARE_API_CLIENT + '&client_secret=' + FOURSQUARE_API_SECRET + '&v=20130815&ll=37.7,-122')
 
     .done(function(data) {
       var response = data.response.venue;
       var description = response.hasOwnProperty('description') ? response.description : '';
 
-
-      var contentString = '<div class="infowindow">'+
-      '<h1>'+ response.name +'</h1>'+ '<div id="pic"><img src="' +
+      var contentString = '<div>'+
+      '<h1>'+ response.name +'</h1>'+ '<div class="infowindow"><img src="' +
       response.bestPhoto.prefix + '210x210' + response.bestPhoto.suffix +
       '" alt="Image Location"></div><p><b>Category: </b>' + response.categories[0].name + '</p><p>' + description + '</p><p><b>Phone: </b>' +
       response.contact.formattedPhone + '</p><p><b>Address: </b> <a target="_blank" href=https://www.google.com/maps/dir/Current+Location/' +
@@ -133,7 +92,14 @@ function listViewModel() {
       '<a target="_blank" href=' + response.canonicalUrl +
       '>Foursquare Page</a></div>';
 
-      place.infoWindow.setContent(contentString);
+
+      google.maps.event.addListener(place.marker, 'click', function(){
+        map.panTo(place.marker.getPosition());
+  			place.marker.setAnimation(google.maps.Animation.BOUNCE);
+  			setTimeout(function(){place.marker.setAnimation(null); }, 1400);
+        infoWindow.setContent(contentString);
+        infoWindow.open(map,place.marker);
+      });
     })
 
     //If the API isn't working, it will alert the user that the foursquare information is unavailable
@@ -142,8 +108,12 @@ function listViewModel() {
       place.infoWindow.setContent(contentString);
     });
 
+    //Resets map position when closing an info window
+    google.maps.event.addListener(infoWindow, 'closeclick', function(){
+      map.fitBounds(mapBounds);
+    });
+
     mapBounds.extend(new google.maps.LatLng(place.lat, place.lng));
-    map.fitBounds(mapBounds);
   });
 
   //Clicking on the list will trigger the map marker
@@ -156,6 +126,7 @@ function listViewModel() {
         self.placesArray().forEach(function(place){
           place.marker.setVisible(true);
           map.fitBounds(mapBounds);
+          infoWindow.close();
         });
     };
 
@@ -172,8 +143,9 @@ function listViewModel() {
         return self.placesArray();
     } else {
         return(ko.utils.arrayFilter(self.placesArray(), function(place) {
-            var visible = stringStartsWith(place.name.toLowerCase(), filter);
+            var visible = (place.name.toLowerCase().indexOf(filter)> -1);
             place.marker.setVisible(visible);
+            infoWindow.close();
             return visible;
         }));
     }
