327f46eeab2eafbf93ba608d41f0caa00f529dac
slkcoin <xcoin@me.com>
2016-06-05 20:51:14 -0700

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp'),
-    ngrok = require('ngrok'),
     connect = require('gulp-connect'),
     uglify = require('gulp-uglify'),
     csso = require('gulp-csso'),
@@ -10,7 +9,7 @@ var gulp = require('gulp'),
     site = '';
 
 gulp.task('build', ['scripts', 'styles','html']);
-gulp.task('serve',['build','connect','ngrok-url']);
+gulp.task('serve',['build','connect']);
 
 gulp.task('scripts', function(){
   gulp.src('src/js/*.js')
@@ -53,12 +52,5 @@ gulp.task('connect', function() {
   connect.server({
     root: 'dist',
     port: 8000,
-  });
-});
-
-gulp.task('ngrok-url', function() {
-  return ngrok.connect(8000, function (err, url) {
-    site = url;
-    console.log('serving your tunnel from: ' + site);
   });
 });
